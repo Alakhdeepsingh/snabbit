8cb3965f7c798c85f4cd68362bb9651675488dc4
Alakhdeep singh <54256747+Alakhdeepsingh@users.noreply.github.com>
2025-06-06 13:24:11 +0530

    fixed deployment issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Header from "./components/Header";
 import Reports from "./components/Reports";
+import SideBar from "./components/SideBar";
 import './App.css';
-import SideBar from "./components/Sidebar";
 
 const App = () => {
   return (
